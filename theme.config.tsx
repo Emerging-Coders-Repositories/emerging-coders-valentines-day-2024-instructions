e356d86f9bc77d7dd7fdd28798f9549a6b628557
Ethan Pineda <joshuapineda66@gmail.com>
2024-02-11 14:05:44 -0600

    pushing current project, rest is up to ari

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>EMCO 2024 Valentines Day Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ethanpaneraa/emerging-coders-valentines-day-2024-instructions',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ethanpaneraa/emerging-coders-valentines-day-2024-instructions',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Emerging Coders Valentines Day 2024 Project',
   },
 }
 
